e92efe22bc689e387245bb5bb523934b7a5cfe8f
qwe123qa <ts01173166@gmail.com>
2016-11-09 15:35:37 +0800

    ha

---
+++ countUp.php
@@ -0,0 +1,14 @@
+<div id="tiny_per"></div>
+
+<script src="js/countUp.js"></script>
+
+<script>
+	var _from=10;
+	var _to=100.15;
+	var _decimal=2;
+	var _time=3;
+
+	_tiny_per = new CountUp("tiny_per", _from, _to, _decimal, _time);
+
+	_tiny_per.start();
+</script>

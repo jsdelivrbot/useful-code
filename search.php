f0fcbf94fbfdcd8079fa2d060a98167718b172d4
qwe123qa <ts01173166@gmail.com>
2016-11-27 02:34:35 +0800

    ha

--- search.php
+++ search.php
@@ -4,34 +4,27 @@ $query_RecWorks="SELECT * FROM client";
 if ($_POST['name']=='' && $_POST['phone']=='' && $_POST['address']=='' && $_POST['company']=='' && $_POST['fruit']=='') {
 	header('Location: client_search.php');
 }else{
-	$query_RecWorks.=" WHERE ";
-}
+	$query_RecWorks.=" WHERE 1=1 ";
+};
 
 $search_array=[
-'name'=>$_POST['name'],
-'phone'=>$_POST['phone'],
-'address'=>$_POST['address'],
-'company'=>$_POST['company'],
-'fruit'=>$_POST['fruit']
+	'name'=>$_POST['name'],
+	'phone'=>$_POST['phone'],
+	'address'=>$_POST['address'],
+	'company'=>$_POST['company'],
+	'fruit'=>$_POST['fruit']
 ];
 
-$plusand=0;
 foreach ($search_array as $key => $value) {
 	if ($value!='') {
-		if ($plusand>0) {
-			$query_RecWorks.=" AND ";
-			if ($key=='name') {
-				$query_RecWorks.="nickname like '%".$_POST['name']."%' OR name like '%".$_POST['name']."%'";
-			}else{
-				$query_RecWorks.=$key." like '%".$value."%'";
-			}
+		$query_RecWorks.=" AND ";
+
+		if ($key=='name') {
+			$query_RecWorks.="nickname like '%".$_POST['name']."%' OR name like '%".$_POST['name']."%'";
+		}else if($key=='fruit'){
+			$query_RecWorks.="ask_fruit like '%".$_POST['fruit']."%' OR book_fruit like '%".$_POST['fruit']."%'";
 		}else{
-			if ($key=='name') {
-				$query_RecWorks.="nickname like '%".$_POST['name']."%' OR name like '%".$_POST['name']."%'";
-			}else{
-				$query_RecWorks.=$key." like '%".$value."%'";
-			}
-			$plusand++;
+			$query_RecWorks.=$key." like '%".$value."%'";
 		}
 	}
 }

1e5ec75de95c8bac7274a9ae33d638ede33abb08
qwe123qa <ts01173166@gmail.com>
2016-12-02 19:18:36 +0800

    ha

--- 過濾圖片+限制字數.php
+++ 過濾圖片+限制字數.php
@@ -1,3 +1,11 @@
+<!-- Ryder再進化之用正規式 -->
+<?php
+strip_tags($row_RecProjects['d_content']);
+preg_match('/.{0,80}/u', strip_tags($row_RecProjects['d_content']), $d_content);
+echo implode($d_content);
+echo "...";
+?>
+
 <!-- Ryder最新完整版 (濾html tag 濾&nbsp;+空白+全形空白)-->
 <?php echo mb_substr(preg_replace("/(&#?[a-z0-9]+;)|(\s+)|(\x{00A0})/u","",strip_tags($row_Recwork['d_content'])) ,0,150,"utf-8"); ?>
 

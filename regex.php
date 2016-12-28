43089b86b1e724763697b08b458ef646438d08fa
qwe123qa <ts01173166@gmail.com>
2016-12-28 17:54:47 +0800

    ha

--- regex.php
+++ regex.php
@@ -1,3 +1,4 @@
+<!-- 取圖 -->
 <?php
 preg_match_all('#<img[^>]*>#i', $row_RecWorks['d_content'], $match);
 
@@ -8,5 +9,21 @@ foreach ($match[0] as $value) {
 	echo $value;
 	echo '</div>';
     if ($i++ == 3) break;
+}
+?>
+
+<!-- 取字 -->
+<?php
+preg_match_all('/.+/u', $row_RecMenuFancy['d_content'], $ch);
+preg_match_all('/.+/u', $row_RecMenuFancy['d_class6'], $en);
+foreach ($ch[0] as $key=>$value) {
+	echo '<li>';
+	echo '<div class="ch dot">';
+	echo $value;
+	echo '</div>';
+	echo '<div class="en">';
+	echo $en[0][$key];
+	echo '</div>';
+	echo '</li>';
 }
 ?>

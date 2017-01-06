356509de5b9b3b5ede2be0949f5d0f55e90c68ce
qwe123qa <ts01173166@gmail.com>
2017-01-06 13:08:37 +0800

    ha

--- Ryder-mobile小常識.php
+++ Ryder-mobile小常識.php
@@ -1,3 +1,27 @@
+<!--=====================================
+=            mobile 鎖定滑動            =
+======================================-->
+<script>
+	document.addEventListener("touchmove", function(event){
+		event.preventDefault();
+	}, false);
+</script>
+
+<!--=================================
+=            偵測 iphone            =
+==================================-->
+<script>
+	function isiPhone(){
+		return (
+			(navigator.platform.indexOf("iPhone") != -1) ||
+			(navigator.platform.indexOf("iPod") != -1)
+			);
+	}
+	if(isiPhone()){
+		alert(1);
+	}
+</script>
+
 <!--============================
 =            字體大小跑掉            =
 =============================-->
--- Ryder小常識.php
+++ Ryder小常識.php
@@ -198,30 +198,6 @@ e.stopPropagation();
 =====================================-->
 http://blog.gtwang.org/web-development/css-font-face/
 
-<!--=====================================
-=            mobile 鎖定滑動            =
-======================================-->
-<script>
-	document.addEventListener("touchmove", function(event){
-		event.preventDefault();
-	}, false);
-</script>
-
-<!--=================================
-=            偵測 iphone            =
-==================================-->
-<script>
-	function isiPhone(){
-		return (
-			(navigator.platform.indexOf("iPhone") != -1) ||
-			(navigator.platform.indexOf("iPod") != -1)
-			);
-	}
-	if(isiPhone()){
-		alert(1);
-	}
-</script>
-
 <!--================================
 =            神奇小教室            =
 =================================-->

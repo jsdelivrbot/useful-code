5d7ded94ef1caf795e1496825c0b765df7dccd3a
qwe123qa <ts01173166@gmail.com>
2017-01-20 20:03:56 +0800

    haha

--- mousewheel.php
+++ mousewheel.php
@@ -61,3 +61,94 @@
 		}
 	});
 </script>
+
+
+<!-- 應用篇 -->
+1. 只有第一頁強制滾一整頁
+
+<style>
+	.index-section-1{
+		z-index: 1;
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+	}
+	.is1-space{
+		position: relative;
+		width: 100%;
+		height: 100%;
+	}
+</style>
+
+<div class="is1-space"></div>
+<div class="index-section-1"></div>
+
+<script>
+	var _delta = 0;
+	var _delta_old = 0;
+	var _nowSection = 1;
+	var _lockScroll = 1;
+
+	function _is1_scroll_reset () {
+		_delta = 0;
+		_delta_old = 0;
+		_lockScroll = 1;
+	}
+
+	$('body').bind('mousewheel', function(event, delta, deltaX, deltaY) {
+		if (_lockScroll) {
+			event.preventDefault();
+		}
+
+		_delta += deltaY;
+		// _delta = (_delta > 0) ? 0 : _delta;
+
+		if (_delta_old > _delta && _nowSection == 1) {
+			// 往下滾
+			_nowSection = 2;
+			_is2_show();
+
+			$(".hamburger").addClass("is-dark");
+			$(".memberLogin").addClass("is-dark");
+			$(".is1-logo").addClass("is-dark");
+
+			TweenMax.to($(".index-section-1"), 1, {
+				top: '-100%',
+				onComplete: function  () {
+					_lockScroll = 0;
+				}
+			});
+
+			TweenMax.to($(".is1-space"), 1, {
+				height: 0,
+				onComplete: function  () {}
+			});
+
+			_delta_old = _delta;
+		}
+		if(_delta > 2 && _nowSection == 2) {
+			// 往上滾
+			_nowSection = 1;
+
+			$(".hamburger").removeClass("is-dark");
+			$(".memberLogin").removeClass("is-dark");
+			$(".is1-logo").removeClass("is-dark");
+
+			TweenMax.to($(".index-section-1"), 1, {
+				top: 0,
+				onComplete: function  () {
+					_is1_scroll_reset();
+				}
+			});
+
+			TweenMax.to($(".is1-space"), 1, {
+				height: '100%',
+				onComplete: function  () {}
+			});
+
+			_delta_old = _delta;
+		}
+	});
+</script>

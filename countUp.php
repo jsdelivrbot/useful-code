3d0b536c8ea1f0652e312a85182f8a30cd5654fb
qwe123qa <ts01173166@gmail.com>
2017-02-14 14:31:58 +0800

    haha

--- countUp.php
+++ countUp.php
@@ -1,3 +1,5 @@
+https://inorganik.github.io/countUp.js/
+
 <div id="tiny_per"></div>
 
 <script src="js/countUp.js"></script>
@@ -7,8 +9,11 @@
 	var _to=100.15;
 	var _decimal=2;
 	var _time=3;
+	var options = {
+		suffix : '%'
+	};
 
-	_tiny_per = new CountUp("tiny_per", _from, _to, _decimal, _time);
+	_tiny_per = new CountUp("tiny_per", _from, _to, _decimal, _time, options);
 
 	_tiny_per.start();
 </script>

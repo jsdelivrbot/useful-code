05921b4ca9a07680f6fb50b460f0e7ee53c4d49f
qwe123qa <ts01173166@gmail.com>
2017-02-22 19:10:52 +0800

    haha

---
+++ 多邊形.php
@@ -0,0 +1,31 @@
+<!-- 六邊 -->
+<style>
+	.pic{
+		margin-bottom: 22px;
+		.hexagon {
+			overflow: hidden;
+			visibility: hidden;
+			transform: rotate(120deg);
+		}
+		.hexagon-in1 {
+			overflow: hidden;
+			transform: rotate(-60deg);
+		}
+		.hexagon-in2 {
+			visibility: visible;
+			transform: rotate(-60deg);
+		}
+		img{
+			width: 100%;
+			height: auto;
+		}
+	}
+</style>
+
+<div class="pic">
+	<div class="hexagon">
+		<div class="hexagon-in1">
+			<div class="hexagon-in2"><img src="images/index/ic-list-01.png"></div>
+		</div>
+	</div>
+</div>

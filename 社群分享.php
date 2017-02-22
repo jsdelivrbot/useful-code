e9cd7dddc6b5cc57e62612e1c6f1f7a6b1690165
qwe123qa <ts01173166@gmail.com>
2017-02-22 15:39:02 +0800

    haha

--- 社群分享.php
+++ 社群分享.php
@@ -1,3 +1,8 @@
+<!-- shareaholic 比較完整 全部的都有(包括line) -->
+https://shareaholic.com/
+
+
+
 <!-- jsSocials -->
 http://js-socials.com/start-using/
 
@@ -37,23 +42,23 @@ http://js-socials.com/start-using/
 
 <!-- 人工 old school -->
 <?php
-$URL='http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+$url='http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
 ?>
 
 <meta property="og:title" content="<?php echo $row_Recwork['d_title']; ?>" />
 <meta property="og:type" content="website" />
-<meta property="og:url" content="<?= $URL ?>" />
+<meta property="og:url" content="<?= $url ?>" />
 <meta property="og:image" content="http://hancure.goods-design.com.tw/images/toplogo-index.png" />
 <meta property="og:description" content="<?php echo $row_Recwork['content']; ?>" />
 
 <!-- fb -->
-<a target="blank" href="http://www.facebook.com/sharer/sharer.php?u=<?= $URL ?>"><img src="images/detaionicon-04.png" width="26"></a>
+<a target="blank" href="http://www.facebook.com/sharer/sharer.php?u=<?= $url ?>"><img src="images/detaionicon-04.png" width="26"></a>
 
 <!-- twitter -->
-<a target="blank" href="http://twitter.com/share?url=<?= $URL ?>"><img src="images/detaionicon-03.png" width="26"></a>
+<a target="blank" href="http://twitter.com/share?url=<?= $url ?>"><img src="images/detaionicon-03.png" width="26"></a>
 
 <!-- pin it -->
-<a target="blank" href="https://www.pinterest.com/pin/create/button/?url=<?= $URL ?>"><img src="images/detaionicon-05.png" width="26"></a>
+<a target="blank" href="https://www.pinterest.com/pin/create/button/?url=<?= $url ?>"><img src="images/detaionicon-05.png" width="26"></a>
 
 <!-- line (手機按才有用)-->
-<a target="blank" href="http://line.naver.jp/R/msg/text/?<?= $URL ?>"><img src="images/detaionicon-02.png" width="26"></a>
+<a target="blank" href="http://line.naver.jp/R/msg/text/?<?= $url ?>"><img src="images/detaionicon-02.png" width="26"></a>

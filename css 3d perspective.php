4f816fd61df3f131f881f7955e9ca70e8fc02b30
qwe123qa <ts01173166@gmail.com>
2017-02-23 13:55:29 +0800

    haha

--- css 3d perspective.php
+++ css 3d perspective.php
@@ -1,2 +1,17 @@
 <!-- 圓形角度計算 -->
 http://www.zhangxinxu.com/wordpress/2012/09/css3-3d-transform-perspective-animate-transition/
+
+
+<!-- 3d hover -->
+<!-- basic concept -->
+http://codepen.io/ariona/pen/JopOOr
+<!-- plugin -->
+https://github.com/ariona/hover3d
+
+<script>
+	$(document).on("mousemove",function(e) {
+		var ax = -($(window).innerWidth()/2- e.pageX)/20;
+		var ay = ($(window).innerHeight()/2- e.pageY)/10;
+		card.attr("style", "transform: rotateY("+ax+"deg) rotateX("+ay+"deg);-webkit-transform: rotateY("+ax+"deg) rotateX("+ay+"deg);-moz-transform: rotateY("+ax+"deg) rotateX("+ay+"deg)");
+	});
+</script>

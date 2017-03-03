11de30e721770d0ae258f7a3f7a16d9f2e671c16
qwe123qa <ts01173166@gmail.com>
2017-03-03 19:18:22 +0800

    haha

--- 過濾圖片+限制字數.php
+++ 過濾圖片+限制字數.php
@@ -1,12 +1,12 @@
+<!-- 這個最簡單好用 -->
+<?= mb_substr(preg_replace("/(\s|\&nbsp\;|　|\xc2\xa0)/", "", strip_tags($row_RecProjects['d_content'])), 0, 90, "utf-8"); ?>
+
 <!-- Ryder再進化之用正規式 -->
 <?php
 preg_match('/.{0,80}/u', trim(strip_tags($row_RecProjects['d_content'])), $d_content);
 echo implode($d_content);
 echo "...";
 ?>
-
-<!-- Ryder最新完整版 (濾html tag 濾&nbsp;+空白+全形空白)-->
-<?php echo mb_substr(preg_replace("/(&#?[a-z0-9]+;)|(\s+)|(\x{00A0})/u","",strip_tags($row_Recwork['d_content'])) ,0,150,"utf-8"); ?>
 
 <!-- filter html tag -->
 <?php echo mb_substr(strip_tags($row_Recwork['d_content']) ,0,25,"utf-8"); ?>

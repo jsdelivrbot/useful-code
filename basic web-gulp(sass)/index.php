a2b91f39d47d40589de70447dfcb95971971d31d
qwe123qa <ts01173166@gmail.com>
2017-03-06 18:44:56 +0800

    haha

---
+++ basic web-gulp(sass)/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>test</title>
+
+	<?php include('meta.php') ?>
+
+	<link rel="stylesheet" href="stylesheets/style.min.css">
+</head>
+
+<body>
+
+</body>
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+<link rel="stylesheet" href="css/all.min.css">
+<script src="js/all.min.js"></script>
+</html>
---
+++ basic web-gulp(sass)/meta.php
@@ -0,0 +1,7 @@
+<meta name="viewport" content="width=device-width" />
+
+<link rel="shortcut icon" href="images/fav.ico" type="image/x-icon">
+<link rel="apple-touch-icon" href="images/fav.ico">
+<link rel="apple-touch-icon" sizes="76x76" href="images/fav.ico">
+<link rel="apple-touch-icon" sizes="120x120" href="images/fav.ico">
+<link rel="apple-touch-icon" sizes="152x152" href="images/fav.ico">

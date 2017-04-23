7bf52623f9caa9e3aecb5faf6d2df9538a8d79b4
qwe123qa <ts01173166@gmail.com>
2017-04-23 22:53:28 +0800

    ha

--- basic web-less/index.php
+++ basic web-less/index.php
@@ -14,6 +14,6 @@
 
 </body>
 
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
 </html>
 

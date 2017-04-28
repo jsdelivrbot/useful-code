816b245ea99092c738a0df60b85930f57f60127e
qwe123qa <ts01173166@gmail.com>
2017-04-29 01:23:12 +0800

    ha

--- pace-preload.php
+++ pace-preload.php
@@ -5,6 +5,17 @@ Pace.trigger('update', this.progress);
 
 <script>
 	Pace.on("update", function(percent){
-		console.log(percent)
+		$(".preloadWrap").stop(true).animate({
+			width: percent + '%'
+		}, 500)
+	});
+
+	Pace.on("done", function(percent){
+		setTimeout(function () {
+			$(".preloadWhiteBlock").hide()
+			$(".preloadWrap .logo").animate({
+				opacity: 1
+			}, 800, preloadend)
+		}, 500)
 	});
 </script>

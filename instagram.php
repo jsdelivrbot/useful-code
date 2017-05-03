7174ecd911cf4585148d58c581ac2f49bdc237a7
qwe123qa <ts01173166@gmail.com>
2017-05-03 18:15:26 +0800

    haha

--- instagram.php
+++ instagram.php
@@ -2,3 +2,29 @@ http://instansive.com/
 
 <!-- hover show like and comment -->
 http://www.uptsi.com/tools/widgets
+
+
+<!-- oembed api -->
+<p>https://www.instagram.com/p/BSveX4gguaq/</p>
+<p>https://www.instagram.com/p/BSveX4gguaq/</p>
+<p>https://www.instagram.com/p/BSveX4gguaq/</p>
+<p>https://www.instagram.com/p/BSveX4gguaq/</p>
+<p>https://www.instagram.com/p/BSveX4gguaq/</p>
+
+<script>
+	$('p').each(function () {
+	    searchText = $(this).text();
+	    matches = searchText.match(/(?:(?:http|https):\/\/)?(?:www.)?(?:instagram.com|instagr.am)\/[p]\/([A-Za-z0-9-_]*)\//g);
+	    if ( matches != null && searchText === matches[0] ) {
+	        $.ajax({
+	            context: this,
+	            url: "https://api.instagram.com/oembed/?url="+matches[0],
+	            dataType: 'jsonp',
+	            async: false,
+	            success: function(data) {
+	            	$(this).html(data.html);
+	            },
+	        });
+	    }
+	});
+</script>

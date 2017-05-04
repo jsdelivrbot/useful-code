36891da1f9a93fab4ec80f24667e2241144d09cd
qwe123qa <ts01173166@gmail.com>
2017-05-04 18:27:23 +0800

    haha

--- mobile_check.php
+++ mobile_check.php
@@ -5,6 +5,10 @@ http://detectmobilebrowsers.com/
 <?php require_once('mobileCheck.php'); ?>
 
 <!-- 以下另存成 mobileCheck.php -->
+ps. 可能會有 header already send 的錯誤
+新增一個 .htaccess 開啟下面這個即可
+php_flag output_buffering on
+因為哥不爽用javascript
 <?php
 require_once 'js/MobileDetect/Mobile_Detect.php';
 

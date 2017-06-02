7fb2489686ad770d3d4cbb9d0812895201e42fa4
qwe123qa <ts01173166@gmail.com>
2017-06-02 19:55:39 +0800

    haha

---
+++ image magnifier.php
@@ -0,0 +1,12 @@
+<script src="js/jquery.imageLens.js"></script>
+
+<div class="phone"><img id="img_01" src="images/products/magnifier@2x.png" width="535"></div>
+
+<script>
+	$("#img_01").imageLens({
+		lensSize: 176,
+		borderSize: 4,
+		borderColor: "#6cc04a",
+		zIndex: 9
+	});
+</script>

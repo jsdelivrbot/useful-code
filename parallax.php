88e152c6cb37197ab32cc367f52588697bc01319
qwe123qa <ts01173166@gmail.com>
2017-06-26 18:41:05 +0800

    haha

--- parallax.php
+++ parallax.php
@@ -4,6 +4,9 @@
 http://codepen.io/scottkellum/pen/bHEcA
 
 
+<!--==============================
+=            scrollit            =
+===============================-->
 https://github.com/rorymurphy/scrollit?utm_source=jquer.in&utm_medium=website&utm_campaign=content-curation
 <script src="js/scrollit.js"></script>
 
@@ -11,7 +14,29 @@ https://github.com/rorymurphy/scrollit?utm_source=jquer.in&utm_medium=website&ut
 	<img src="images/news-detail-leaf-1.png" width="201" data-parallax="0.4" style="position: relative;">
 </div>
 
+or
 
+<script>
+	$('.ia-article').parallax({
+		axis: 'y',
+		attr: 'top',
+		speed: -0.1
+	});
+
+	$('.ia-article').tween({
+		start: 0,
+		end: $(window).height() * 0.4,
+		easing: 'easeInCubic',
+		styles: {
+			'opacity': {
+				startValue: '0',
+				endValue: '1'
+			}
+		}
+	});
+</script>
+
+<!-- with waypoint -->
 <script>
 	$(".m-athletesList").waypoint({
 		position: -$(window).height()*0.8,

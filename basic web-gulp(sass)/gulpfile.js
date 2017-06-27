d22c524c5eb84125458cdfc4c5b4c2d67ebb0c05
qwe123qa <ts01173166@gmail.com>
2017-06-27 16:58:14 +0800

    haha

--- basic web-gulp(sass)/gulpfile.js
+++ basic web-gulp(sass)/gulpfile.js
@@ -1,22 +1,33 @@
 var gulp = require('gulp');
 var gulpSass = require('gulp-sass');
 var gulpJade = require('gulp-jade-php');
+var gulpSourcemaps = require('gulp-sourcemaps');
+var gulpAutoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', function() {
-	gulp.src('sass/*.sass')
-		.pipe(gulpSass())
-		.pipe(gulp.dest('stylesheets'));
+    gulp.src('sass/*.sass')
+        .pipe(gulpSass({
+            errLogToConsole: true,
+            outputStyle: 'compressed'
+        }))
+        .pipe(gulpSourcemaps.init())
+        .pipe(gulpAutoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulpSourcemaps.write())
+        .pipe(gulp.dest('stylesheets'));
 });
 
 gulp.task('jade', function() {
-  gulp.src('jade/*.jade')
-    .pipe(gulpJade({
-    	//
-    }))
-     .pipe(gulp.dest('./'));
+    gulp.src('jade/*.jade')
+        .pipe(gulpJade({
+            // pretty: true
+        }))
+        .pipe(gulp.dest('./'));
 });
 
-gulp.task('watch', function () {
+gulp.task('watch', function() {
     gulp.watch('sass/*.sass', ['sass']);
     gulp.watch('jade/*.jade', ['jade']);
 });

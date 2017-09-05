d14de1c3c8d5ff512860cee0a2393c25794b3092
qwe123qa <ts01173166@gmail.com>
2017-09-05 19:50:01 +0800

    haha

--- youtube.php
+++ youtube.php
@@ -137,11 +137,11 @@ https://developers.google.com/youtube/player_parameters#start
 <li onclick="youtube_play(this);" data-youtube="<?= $row_Recvideoimage['d_class4'] ?>"><img src="<?php echo $row_Recvideoimage['file_link1'] ; ?>"><img src="img/videoplay.png" width="80" class="videoplay"></li>
 
 <script>
-	function youtube_play (e) {
-	  var $this=$(e);
-	  var _src=$this.data("youtube");
-	  var _w=$this.width() + "px";
-	  var _h=$this.height() + "px";
-	  $this.html('<iframe src="http://www.youtube.com/embed/'+_src+'?autoplay=1" frameborder="0" allowfullscreen width="'+_w+'" height="'+_h+'"></iframe>');
+	function youtube_play(e) {
+		var $this = $(e);
+		var _src = $this.data("youtube");
+		var _w = $this.width() + "px";
+		var _h = $this.height() + "px";
+		$this.html('<iframe src="http://www.youtube.com/embed/' + _src + '?autoplay=1" frameborder="0" allowfullscreen width="' + _w + '" height="' + _h + '"></iframe>');
 	}
 </script>

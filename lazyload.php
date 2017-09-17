6f68acf2b0b7a0fda420e6bc98883890860e495b
qwe123qa <ts01173166@gmail.com>
2017-09-17 08:00:46 +0800

    ha

---
+++ lazyload.php
@@ -0,0 +1,11 @@
+https://github.com/eisbehr-/jquery.lazy/tree/master/plugins
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.lazy/1.7.4/jquery.lazy.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.lazy/1.7.4/jquery.lazy.plugins.min.js"></script>
+
+<script>
+	$('.lazy').lazy({
+		effect: "fadeIn",
+		effectTime: 1000,
+	});
+</script>

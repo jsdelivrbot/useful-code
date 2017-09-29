69130e09479d381598c12a8cb72185272f22555f
qwe123qa <ts01173166@gmail.com>
2017-09-29 12:31:10 +0800

    haha

---
+++ animate.css + wow.php
@@ -0,0 +1,13 @@
+https://daneden.github.io/animate.css/
+https://github.com/matthieua/WOW
+
+<div class="deco-1 wow fadeInLeft" data-wow-duration="2s"><img src="images/featurewheat-deco-1.png"></div>
+
+<link rel="stylesheet" href="css/animate.css">
+<script src="js/wow.js"></script>
+
+<script>
+	var wow = new WOW({
+		callback: function (el) {}
+	}).init();
+</script>

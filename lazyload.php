b1a4439fb39e4667b76a1779cf04d3848eef47c3
qwe123qa <ts01173166@gmail.com>
2017-10-11 21:14:56 +0800

    ha

--- lazyload.php
+++ lazyload.php
@@ -4,9 +4,14 @@ https://github.com/eisbehr-/jquery.lazy/tree/master/plugins
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.lazy/1.7.4/jquery.lazy.plugins.min.js"></script>
 
 <script>
-	$('.lazy').lazy({
+	// 有 chainable: false 這個lazyload才有實例(才可以用update)
+	var lazyload = $('.lazy').lazy({
+		chainable: false,
 		effect: "fadeIn",
 		effectTime: 1000,
 		defaultImage: 'images/lazy-default.svg',
 	});
+
+	// 開燈箱load不到時用
+	lazyload.update();
 </script>

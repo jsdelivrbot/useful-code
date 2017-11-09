eb63000b667a5fb405aaf40b57ab28e109757fc9
qwe123qa <ts01173166@gmail.com>
2017-11-09 14:45:53 +0800

    haha

--- scrollTo+go top.php
+++ scrollTo+go top.php
@@ -1,25 +1,25 @@
-
 <!-- scroll to -->
+https://github.com/flesler/jquery.scrollTo
+
 <script src="js/jquery.scrollTo.js"></script>
 
 <script>
 	$(".knowmore").mousedown(function() {
-		$.scrollTo( $('.index-area3'), {duration:1000} );
+		$.scrollTo( $('.index-area3'), {duration: 1000} );
 	});
 
 	// offset 用法
 	$("#goroom").mousedown(function() {
-		$.scrollTo( $('#room'), {duration:1000 , offset: -40} );
+		$.scrollTo( $('#room'), {duration: 1000 , offset: -40} );
 	});
 </script>
-
 
 
 <!-- window內建 -->
 <!-- go top -->
 <script>
 	$(".backtotop").click(function(){
-		$("html,body").animate({scrollTop:0 },1000);
+		$("html,body").animate({scrollTop: 0 },1000);
 	});
 	$(window).scroll(function() {
 		if ( $(this).scrollTop() > 300){
@@ -30,11 +30,12 @@
 	});
 </script>
 
+
 <!-- 很多個gotop -->
 <script>
 	$("body").find('div').find('#backtotop').mousedown(function(){
 		$("html,body").animate({
-			scrollTop:0
+			scrollTop: 0
 		},1000);
 	});
 </script>

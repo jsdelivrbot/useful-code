d9872ad8ac317551e64f80a93bf7d6416a6c5a53
qwe123qa <ts01173166@gmail.com>
2017-12-05 18:27:02 +0800

    haha

--- vegas.php
+++ vegas.php
@@ -45,7 +45,38 @@
 <!--=============================
 =            pager            =
 =============================-->
-<script type="text/javascript">
+<script>
+    $(".vegas-banner").vegas({
+        timer: false,
+        delay: 5500,
+        transitionDuration: 1800,
+        slides: [{
+            src: "images/technic-banner.jpg"
+        },{
+            src: "images/technic-banner-2.jpg"
+        },{
+            src: "images/technic-banner-3.jpg"
+        },{
+            src: "images/technic-banner-4.jpg"
+        },{
+            src: "images/technic-banner-5.jpg"
+        }],
+        init: function (globalSettings) {
+            let slides = $(".contact-banner").vegas('options', 'slides').length;
+            while(slides--){
+                $("<a href='#0'>").appendTo($(".banner-dots"));
+            }
+        },
+        walk: function (index, slideSettings) {
+            vegaspager();
+            $(".vegas-slide-inner").css({
+                'background-position-y': 50 + _scrollTop * -0.2 + '%'
+            })
+        }
+    });
+</script>
+
+<script>
     $(".fish li").click(function () {
         var index = $(this).index();
         $(".banner").vegas('jump', index);

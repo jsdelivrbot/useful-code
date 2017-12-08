0ccd612ca987ff04004027baec4f7948918e7356
qwe123qa <ts01173166@gmail.com>
2017-12-08 12:54:32 +0800

    haha

--- jquery easing.php
+++ jquery easing.php
@@ -3,4 +3,5 @@ http://gsgd.co.uk/sandbox/jquery/easing/
 <script src="js/jquery.easing.1.3.js"></script>
 <script src="js/jquery.easing.compatibility.js"></script>
 
+<!-- 只有4個參數 -->
 http://gizma.com/easing/#circ2

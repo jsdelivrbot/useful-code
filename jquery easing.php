96469de004e87752eacdf16ca9716734a9fd5073
qwe123qa <ts01173166@gmail.com>
2017-12-08 12:54:11 +0800

    haha

--- jquery easing.php
+++ jquery easing.php
@@ -2,3 +2,5 @@ http://gsgd.co.uk/sandbox/jquery/easing/
 
 <script src="js/jquery.easing.1.3.js"></script>
 <script src="js/jquery.easing.compatibility.js"></script>
+
+http://gizma.com/easing/#circ2

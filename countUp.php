e714c877684038cacf30909fb30640f1c9cb618b
qwe123qa <ts01173166@gmail.com>
2017-12-08 12:57:34 +0800

    haha

--- countUp.php
+++ countUp.php
@@ -10,6 +10,13 @@ https://inorganik.github.io/countUp.js/
 	var _decimal=2;
 	var _time=3;
 	var options = {
+		useEasing: true,
+		easingFn: function(t, b, c, d) {
+			t /= d/2;
+			if (t < 1) return -c/2 * (Math.sqrt(1 - t*t) - 1) + b;
+			t -= 2;
+			return c/2 * (Math.sqrt(1 - t*t) + 1) + b;
+		},
 		suffix : '%'
 	};
 

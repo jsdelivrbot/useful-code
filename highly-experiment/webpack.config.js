f09fe8aba215e747fa5c0e3254d31d77adc73743
qwe123qa <ts01173166@gmail.com>
2018-01-12 22:25:59 +0800

    haha

--- highly-experiment/webpack.config.js
+++ highly-experiment/webpack.config.js
@@ -2,15 +2,26 @@ var path = require('path');
 var entries = require('webpack-entries');
 var autoprefixer = require('autoprefixer');
 
-// vue
 module.exports = {
-    entry: entries('./vue/*.js', true),
+    entry: entries('./src/*.js', true),
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].js'
     },
     module: {
         rules: [{
+            test: /\.js/,
+            exclude: /(node_modules|bower_components)/,
+            use: [{
+                loader: 'babel-loader',
+                options: {
+                    presets: ['es2015'],
+                    plugins: [
+                        'async-to-promises'
+                    ]
+                }
+            }]
+        }, {
             test: /\.vue$/,
             use: [{
                 loader: 'vue-loader',
@@ -32,44 +43,3 @@ module.exports = {
         }
     }
 };
-
-// react
-// module.exports = {
-//     entry: './components/all.jsx',
-//     output: {
-//         path: path.resolve(__dirname, 'dist'),
-//         filename: 'bundle.js'
-//     },
-//     module: {
-//         rules: [{
-//             test: /\.scss$/,
-//             use: [{
-//                 loader: 'style-loader'
-//             }, {
-//                 loader: 'css-loader',
-//                 // options: {
-//                 //     modules: true
-//                 // }
-//             }, {
-//                 loader: 'autoprefixer-loader',
-//                 options: {
-//                     browsers: [
-//                         'last 2 versions',
-//                         'ie >= 9',
-//                         'and_chr >= 2.3'
-//                     ]
-//                 }
-//             }, {
-//                 loader: 'sass-loader'
-//             }]
-//         }, {
-//             test: /\.jsx$/,
-//             use: [{
-//                 loader: 'babel-loader',
-//                 options: {
-//                     presets: ['es2015', 'react']
-//                 }
-//             }],
-//         }]
-//     },
-// };

10205780290361d49c135f50ac5aea7a15fd70b7
qwe123qa <ts01173166@gmail.com>
2018-01-22 13:16:33 +0800

    haha

--- video tag.php
+++ video tag.php
@@ -6,15 +6,22 @@
 		position: relative;
 		width: 100%;
 		height: 585px;
-		video{
+		.video-container{
+			position: absolute;
+			top: -50%;
+			left: -50%;
+			width: 200%;
+			height: 200%;
+		}
+		#video{
 			position: absolute;
 			top: 0;
 			bottom: 0;
 			right: 0;
 			left: 0;
 			margin: auto;
-			min-height: 100%;
-			min-width: 100%;
+			min-height: 50%;
+			min-width: 50%;
 		}
 	}
 

1c893724ec1acf2e746ec6745965dbf7fd9a9ebe
qwe123qa <ts01173166@gmail.com>
2018-04-26 15:01:50 +0800

    haha

---
+++ php framework slim.php
@@ -0,0 +1,28 @@
+https://www.slimframework.com/docs/v2/start/get-started.html
+
+<!-- composer -->
+composer require slim/slim:~2.0
+
+<!-- .htaccess -->
+RewriteEngine On
+RewriteCond %{REQUEST_FILENAME} !-f
+RewriteCond %{REQUEST_FILENAME} !-d
+RewriteRule ^ index.php [QSA,L]
+
+<!-- index.php -->
+<?php
+require 'vendor/autoload.php';
+
+$config = [
+	'templates.path' => 'views'
+];
+
+$app = new \Slim\Slim($config);
+
+$app->get('/hello/:id', function ($id) use ($app) {
+    $app->render('html.php', [
+    	'id' => $id
+    ]);
+});
+
+$app->run();

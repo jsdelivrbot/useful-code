3b42f0d326423075ae382c306611802f025f33d8
qwe123qa <ts01173166@gmail.com>
2018-05-10 17:42:57 +0800

    haha

---
+++ 常用slider.php
@@ -0,0 +1,15 @@
+<!-- bxslider -->
+https://bxslider.com/
+
+<!-- slick slider -->
+https://github.com/kenwheeler/slick/
+
+<!-- pro slider -->
+https://github.com/bqworks/slider-pro
+
+<!-- swiper slider -->
+http://idangero.us/swiper/api/#.WdoAmVuCyUk
+
+<!-- flickity -->
+parallax好像不錯
+https://flickity.metafizzy.co/

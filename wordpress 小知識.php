3fd37efa8fbe649b600367b9def813d22e36a09e
qwe123qa <ts01173166@gmail.com>
2018-06-04 18:02:15 +0800

    wordpress pods

--- wordpress 小知識.php
+++ wordpress 小知識.php
@@ -1,3 +1,43 @@
+<!--=============================
+=            自製內頁            =
+==============================-->
+名字改成 single-{post_type}.php
+
+<?php
+$pods = pods( 'projects', get_the_id() );
+$title = $pods->field('project_title');
+$title_en = $pods->field('project_title_en');
+$cover = $pods->field('project_cover');
+$row = $pods->row();
+?>
+
+
+<!--=================================
+=            自製pods首頁            =
+==================================-->
+用套件Post Types Order排序
+
+<?php
+$param = [
+	"orderby" => "menu_order ASC"
+];
+
+$pods = pods( 'projects', $param );
+
+// $terms = get_cat_name( 2 );
+// echo '<pre>'; print_r($terms); echo '</pre>';
+
+while ($pods->fetch()) {
+	$title = $pods->field('project_title');
+	$title_en = $pods->field('project_title_en');
+	$cover = $pods->field('project_cover');
+
+	$row = $pods->row();
+	echo '<pre>'; print_r($row); echo '</pre>';
+}
+
+?>
+
 <!--========================================
 =            新增文章分類 (待研究)           =
 =========================================-->
@@ -9,6 +49,7 @@ advance custom filed
 =            自製模版            =
 ==============================-->
 https://www.zhihu.com/question/20129430
+
 <?php
 /*
 名字改成 page-template-xx.php
@@ -26,6 +67,7 @@ Template Name: xx 模板
 =            轉址到子目錄            =
 ==================================-->
 wp 設定 一般 網站位址（URL）也要改
+
 <IfModule mod_rewrite.c>
 RewriteEngine on
 RewriteCond %{HTTP_HOST} ^(www.)?ryderisgood.com$

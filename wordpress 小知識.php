2e9e93e4d5cf687d4ab0481fbbf92b43e20cd3fc
qwe123qa <ts01173166@gmail.com>
2018-06-07 13:08:15 +0800

    haha

--- wordpress 小知識.php
+++ wordpress 小知識.php
@@ -1,3 +1,43 @@
+<!--=============================================
+=            賞你一個乾淨的 wp_head()            =
+==============================================-->
+把這串加在 functions.php
+
+<?php
+//刪除wp_head()多餘的代碼
+remove_action( 'wp_head', 'feed_links_extra', 3 ); //去除評論feed
+remove_action( 'wp_head', 'feed_links', 2 ); //去除文章feed
+remove_action( 'wp_head', 'rsd_link' ); //針對Blog的遠程離線編輯器接口
+remove_action( 'wp_head', 'wlwmanifest_link' ); //Windows Live Writer接口
+remove_action( 'wp_head', 'index_rel_link' ); //移除當前頁面的索引
+remove_action( 'wp_head', 'parent_post_rel_link', 10, 0 ); //移除後面文章的url
+remove_action( 'wp_head', 'start_post_rel_link', 10, 0 ); //移除最開始文章的url
+remove_action( 'wp_head', 'wp_shortlink_wp_head', 10, 0 );//自動生成的短鏈接
+remove_action( 'wp_head', 'adjacent_posts_rel_link', 10, 0 ); ///移除相鄰文章的url
+remove_action( 'wp_head', 'wp_generator' ); // 移除版本號
+remove_action( 'wp_head', 'wp_print_styles', 8 );
+remove_action( 'wp_head', 'wp_print_head_scripts', 9 );
+//remove_action( 'wp_head', 'rel_canonical' );
+remove_action( 'wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0 );
+remove_action( 'wp_head', 'print_emoji_detection_script',7 );
+remove_action( 'wp_head', 'rest_output_link_wp_head', 10 );
+remove_action( 'wp_head', 'wp_oembed_add_discovery_links', 10 );
+remove_action( 'wp_head', 'wp_resource_hints', 2 );
+
+//移除wordpress頂部工具欄css樣式media="screen"
+function my_admin_bar_init() {
+    remove_action( 'wp_head', '_admin_bar_bump_cb' );
+}
+add_action( 'admin_bar_init', 'my_admin_bar_init' );
+
+//移除頭部多餘.recentcomments樣式
+function Fanly_remove_recentcomments_style() {
+    global $wp_widget_factory;
+    remove_action( 'wp_head', array( $wp_widget_factory->widgets['WP_Widget_Recent_Comments'], 'recent_comments_style' ) );
+}
+add_action( 'widgets_init', 'Fanly_remove_recentcomments_style' );
+?>
+
 <!--=============================
 =            分類操作            =
 ==============================-->

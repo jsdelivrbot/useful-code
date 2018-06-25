f4187e4c1119af0269b029293f0db0246276242a
ryder <ts01173166@gmail.com>
2018-06-25 20:18:48 +0800

    haha

---
+++ Ryder-linux小常識.php
@@ -0,0 +1,9 @@
+<!--==================================================
+=            ubuntu 16.04 install mongodb            =
+===================================================-->
+https://www.digitalocean.com/community/tutorials/how-to-install-mongodb-on-ubuntu-16-04
+
+<!--===================================================
+=            ubuntu 16.04 install node 9.x            =
+====================================================-->
+https://www.rosehosting.com/blog/how-to-install-node-js-on-ubuntu-16-04/

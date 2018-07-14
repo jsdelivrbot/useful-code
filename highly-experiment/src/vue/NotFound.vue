4bd724b92a2b589996b0f6ddc8d4faebd66546da
qwe123qa <ts01173166@gmail.com>
2018-07-15 05:51:18 +0800

    ha

--- highly-experiment/src/vue/NotFound.vue
+++ highly-experiment/src/vue/NotFound.vue
@@ -1,6 +1,17 @@
 <template lang="html">
-    <div>PAGE NOT FOUND</div>
+    <div class="notfound grid-y align-center-middle">PAGE NOT FOUND</div>
 </template>
+
+<style lang="scss" scoped>
+	.notfound{
+		font-family: 'Roboto', sans-serif;
+		font-size: 40px;
+		letter-spacing: 2px;
+		color: #1e1e1e;
+		text-align: center;
+		height: 100vh;
+	}
+</style>
 
 <script>
     export default {

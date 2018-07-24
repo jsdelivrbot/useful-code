16db8ce1180a6a54cd35358fed0c5cb3ff0e3f4c
ryder <ts01173166@gmail.com>
2018-07-24 13:49:15 +0800

    ha

--- Ryder-cms小常識.php
+++ Ryder-cms小常識.php
@@ -1,3 +1,11 @@
+<!--==============================================
+=            mysql 5.7 group by error            =
+===============================================-->
+https://stackoverflow.com/questions/23921117/disable-only-full-group-by
+
+phpmyadmin(上方選單列)->variables->sql mode->only_full_group_by 移除
+
+
 <!--==============================================================
 =            編輯器 d_content source image src replace            =
 ===============================================================-->

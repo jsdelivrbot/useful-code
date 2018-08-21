53ced829567750b1960fea06c95024128a257406
ryder <ts01173166@gmail.com>
2018-08-21 14:47:59 +0800

    ha

--- 3D hover.php
+++ 3D hover.php
@@ -4,7 +4,13 @@ https://github.com/gijsroge/tilt.js
 <script src="https://unpkg.com/tilt.js@1.2.1/dest/tilt.jquery.min.js"></script>
 
 <div class="cell large-3 pic-container"><a href="products_detail.php">
-	<div class="rotate-area" data-tilt data-tilt-perspective="500" data-tilt-speed="2000" data-tilt-scale="0.9" data-tilt-glare="true" data-tilt-maxGlare="0.3">
+	<div class="rotate-area"
+		data-tilt
+		data-tilt-perspective="500"
+		data-tilt-speed="2000"
+		data-tilt-scale="0.9"
+		data-tilt-glare="true"
+		data-tilt-maxGlare="0.3">
 		<div class="pic" style="background: url(images/series-2.jpg) center center / cover no-repeat"></div>
 		<div class="title">CS3303</div>
 	</div>

487531e5cf8c35552ab3b492e3b5ee1653eca601
qwe123qa <ts01173166@gmail.com>
2018-11-05 12:53:06 +0800

    ha

--- 模糊變清楚.php
+++ 模糊變清楚.php
@@ -8,3 +8,39 @@
 		offset: 550
 	})
 </script>
+
+
+
+<!-- canvas -->
+<canvas id="canvas-sock" width="300" height="247"></canvas>
+
+<script>
+	var scale = {
+		value: 25
+	};
+
+	var canvas = document.getElementById("canvas-sock");
+	var ctx = canvas.getContext("2d");
+	ctx.imageSmoothingEnabled = false;
+
+	var cw = canvas.width;
+	var ch = canvas.height;
+
+	var img = new Image();
+	img.src = "images/sockforcanvas.jpg";
+
+	function sock_pixel() {
+		TweenMax.to(scale, 2, {
+			value: 1,
+			ease: Linear.easeNone,
+			onUpdate: sock_update
+		});
+	}
+
+	function sock_update() {
+		var sw = cw / scale.value;
+		var sh = ch / scale.value;
+		ctx.drawImage(img, 0, 0, sw, sh);
+		ctx.drawImage(canvas, 0, 0, sw, sh, 0, 0, cw, ch);
+	}
+</script>

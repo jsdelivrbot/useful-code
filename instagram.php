0b82616da6777ec3bcd055d41fba5e71107e1eb5
qwe123qa <ts01173166@gmail.com>
2018-11-05 12:17:27 +0800

    ha

--- instagram.php
+++ instagram.php
@@ -4,7 +4,32 @@ http://instansive.com/
 http://www.uptsi.com/tools/widgets
 
 
+
 <!-- oembed api -->
+<section class="about-instagram">
+	<div class="item" data-ig="BmSDmhplPZE"></div>
+	<div class="item" data-ig="BjwdFbkFH98"></div>
+	<div class="item" data-ig="BmuzEqaF1UZ"></div>
+	<div class="item" data-ig="BnMCkM0FpHv"></div>
+	<div class="item" data-ig="BkJm9qplQuL"></div>
+	<div class="item" data-ig="Blt51NLlZNP"></div>
+</section>
+
+<script>
+	$('.about-instagram .item').each(function (i, el) {
+
+		var _url = "https://api.instagram.com/oembed?url=http://instagr.am/p/" + el.dataset.ig;
+
+		$.get(_url, function(result){
+			$(el).html(result.html);
+		});
+
+	});
+</script>
+
+
+
+<!-- 之前用的  會有"跨來源資源共用（CORS）"的問題....似乎 -->
 <p>https://www.instagram.com/p/BSveX4gguaq/</p>
 <p>https://www.instagram.com/p/BSveX4gguaq/</p>
 <p>https://www.instagram.com/p/BSveX4gguaq/</p>
